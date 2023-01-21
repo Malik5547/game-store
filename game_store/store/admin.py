8f7374f82e42cbf11bd2a1892b2a98e993e49ef1
Peter <petru.balan.54@gmail.com>
2023-01-21 19:42:07 +0200

    added test index page

--- game_store/store/admin.py
+++ game_store/store/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Product
+from .models import Product, ProductCategory, Order
 
 # Register your models here.
+admin.site.register(ProductCategory)
 admin.site.register(Product)
+admin.site.register(Order)
--- game_store/store/views.py
+++ game_store/store/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
+from .models import ProductCategory
 
 # Create your views here.
-def index(requse):
-    return HttpResponse("Welcome to the best video game store.")
+def index(request):
+    categories = ProductCategory.objects.order_by('name')
+    context = {
+        'categories_list': categories,
+    }
+    return render(request, 'store/index.html')
+
+
+def product(request, product_id):
+    return HttpResponse(f"This is product {product_id}")
