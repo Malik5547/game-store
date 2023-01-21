28c794c05f702a5510755327ed2f88e43a87b25e
Peter <petru.balan.54@gmail.com>
2023-01-21 12:58:41 +0200

    created models

--- game_store/store/models.py
+++ game_store/store/models.py
@@ -1,3 +1,29 @@
+import datetime
+
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class ProductCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    price = models.FloatField(default=0)
+    desc = models.CharField(max_length=250)
+    thumb = models.ImageField()
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    stock = models.IntegerField(default=0)
+
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.FloatField(default=0)
+    amount = models.IntegerField(default=1)
+    date = models.DateTimeField(default=datetime.datetime.now())
