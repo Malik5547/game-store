6d9a37747001cac4751ed8771ab3e942c777217c
Peter <petru.balan.54@gmail.com>
2023-01-24 12:58:36 +0200

    changed main page and added product page

--- game_store/store/urls.py
+++ game_store/store/urls.py
@@ -5,5 +5,10 @@ from . import views
 app_name = 'store'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('sign-up', views.sign_up, name='sign-up'),
+    path('sign-in', views.sign_in, name='sign-in'),
+    path('logout', views.log_out, name='logout'),
+    path('cat/<int:cat_id>', views.category, name='category'),
+    path('product/<int:product_id>', views.product, name='category'),
     path('<int:product_id>', views.product, name='product'),
 ]
--- game_store/store/views.py
+++ game_store/store/views.py
@@ -1,12 +1,14 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
 
+from .forms import RegistrationForm, LoginForm
 from .models import ProductCategory, Product
 
 # Create your views here.
 def index(request):
     categories = ProductCategory.objects.order_by('name')
-    products = Product.objects.order_by('id')[:6]
+    products = Product.objects.order_by('id')
     context = {
         'categories_list': categories,
         'product_list': products,
@@ -14,5 +16,61 @@ def index(request):
     return render(request, 'store/index.html', context)
 
 
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'store/sign_up.html', {'form': form})
+
+
+def sign_in(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+
+            print("User logged.")
+
+            return redirect('/')
+
+        print("Wrong credentials.")
+    else:
+        form = LoginForm()
+
+    return render(request, 'store/sign_in.html', {'form': form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect('/')
+
+
+def category(request, cat_id):
+    categories = ProductCategory.objects.order_by('name')
+    products_by_cat = Product.objects.filter(category=cat_id)
+    cat = ProductCategory.objects.get(id=cat_id)
+
+    context = {
+        'categories_list': categories,
+        'selected_cat': cat,
+        'product_list': products_by_cat,
+    }
+
+    return render(request, 'store/index.html', context)
+
+
 def product(request, product_id):
-    return HttpResponse(f"This is product {product_id}")
+    selected_product = Product.objects.get(id=product_id)
+
+    context = {
+        'product': selected_product,
+    }
+
+    return render(request, 'store/product.html', context)
