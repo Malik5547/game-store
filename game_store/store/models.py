a6e2c3e5ed748ee26ae3d09855f28d531f71b7de
Peter <petru.balan.54@gmail.com>
2023-01-26 14:02:27 +0200

    model field names changed

--- game_store/store/models.py
+++ game_store/store/models.py
@@ -10,6 +10,14 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=50)
     icon = models.ImageField(upload_to='images', default="/thumb.png")
 
+    def get_games_count(self):
+        games = Product.objects.filter(category=self.id)
+        return games.count()
+    get_games_count.short_description = "Games"
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
@@ -19,13 +27,29 @@ class Product(models.Model):
     thumb = models.ImageField(upload_to='images')
     image1 = models.ImageField(upload_to='images', default=None)
     image2 = models.ImageField(upload_to='images', default=None)
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategory, null=False, on_delete=models.CASCADE)
+
+    def get_category(self):
+        return self.category
+    get_category.short_description = 'Category'
 
 
 class Purchase(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.FloatField(default=0)
     amount = models.IntegerField(default=1)
     date = models.DateTimeField(default=datetime.datetime.now())
+
+    def get_user(self):
+        return self.user
+    get_user.short_description = "User"
+
+    def get_product(self):
+        return self.product.name
+    get_product.short_description = "Product"
+
+    def get_price(self):
+        return f"${self.price}"
+    get_price.short_description = "Price"
